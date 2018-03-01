cfb81cb897de39d72e0f1dd6a9ae192da3569b6c
Maor Assayag <maorassayag16@gmail.com>
2018-03-01 12:37:10 +0200

    update

---
+++ C Sharp with Unity/SolarSystem education/Assets/Scripts/aboutPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class aboutPanel : MonoBehaviour {
+    bool state = false;
+    public GameObject Panel;
+
+    void Start () {
+        Panel.SetActive(false);
+    }
+
+    void Update () {
+		
+	}
+
+    public void OnClick()
+    {
+        state = !state;
+        Panel.SetActive(state);
+    }
+}
