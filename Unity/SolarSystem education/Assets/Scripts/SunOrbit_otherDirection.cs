e5e9687ed657b79e5231cf2fd160c29ded5a3eee
Maor Assayag <maorassayag16@gmail.com>
2018-03-01 12:38:56 +0200

    update

---
+++ Unity/SolarSystem education/Assets/Scripts/SunOrbit_otherDirection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunOrbit_otherDirection : MonoBehaviour {
+    public float rotspeed = 0.1f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(-transform.up, rotspeed, Space.World);
+    }
+}
---
+++ Unity/SolarSystem education/Assets/Scripts/factPanel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class factPanel : MonoBehaviour {
+    bool state = false;
+    public GameObject Panel;
+    bool infoPressed = false;
+
+    // Use this for initialization
+    void Start () {
+        Panel.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {	
+	}
+
+    public void OnClick()
+    {
+        if (!infoPressed) { infoPressed = !infoPressed; }
+        state = !state;
+        Panel.SetActive(state);
+    }
+
+    public void MenuPressed()
+    {
+       if (!state && !infoPressed)
+        {
+            OnClick();
+        }
+    }
+}
